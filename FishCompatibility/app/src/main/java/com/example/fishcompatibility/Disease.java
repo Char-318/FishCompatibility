c39eb97b8da744982af5053930cfcaf5730f98f5
char-318 <charlotte@mulvey.uk.net>
2023-02-25 12:12:44 +0000

    Populating Disease class

--- FishCompatibility/app/src/main/java/com/example/fishcompatibility/Disease.java
+++ FishCompatibility/app/src/main/java/com/example/fishcompatibility/Disease.java
@@ -1,4 +1,28 @@
 package com.example.fishcompatibility;
 
 public class Disease {
+    private final String name;
+    private final String[] aliases;
+    private final String[] symptoms;
+    private final String[] causes;
+    private final String[] prevention;
+    private final String[] treatment;
+
+    public Disease(String pName, String[] pAliases, String[] pSymptoms, String[] pCauses,
+                   String[] pPrevention, String[] pTreatment) {
+        name = pName;
+        aliases = pAliases;
+        symptoms = pSymptoms;
+        causes = pCauses;
+        prevention = pPrevention;
+        treatment = pTreatment;
+
+    }
+
+    public String getName() { return name; }
+    public String[] getAliases() { return aliases; }
+    public String[] getSymptoms() { return symptoms; }
+    public String[] getCauses() { return causes; }
+    public String[] getPrevention() { return prevention; }
+    public String[] getTreatment() { return treatment; }
 }
