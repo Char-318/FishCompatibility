9d22558064ddf9d7e80afb190723834e7c45ec0a
char-318 <charlotte@mulvey.uk.net>
2023-03-25 19:29:07 +0000

    Displaying list of fish as they are selected from dropdown

--- FishCompatibility/app/src/main/java/com/example/fishcompatibility/TankActivity.java
+++ FishCompatibility/app/src/main/java/com/example/fishcompatibility/TankActivity.java
@@ -1,15 +1,21 @@
  package com.example.fishcompatibility;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -30,13 +36,14 @@ import java.util.ArrayList;
         // Populating arrays
         Parcelable[] parcelable = getIntent().getExtras().getParcelableArray("fishes");
         fishes = new Fish[parcelable.length];
-        fishNames = new String[parcelable.length];
+        fishNames = new String[parcelable.length + 1];
+        fishNames[0] = "Select a fish...";
 
         for (int i = 0; i < parcelable.length; i++) {
             Fish fish = (Fish) parcelable[i];
             String fishName = fish.getName();
             fishes[i] = fish;
-            fishNames[i] = fishName;
+            fishNames[i + 1] = fishName;
         }
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, fishNames);
@@ -47,22 +54,30 @@ import java.util.ArrayList;
 
      @Override
      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-         tankFish.add(fishes[i]);
+         if (i != 0) {
+             tankFish.add(fishes[i - 1]);
 
-         FishFragment fragment = (FishFragment) getSupportFragmentManager()
-                 .findFragmentByTag("fishFragment");
+             FishFragment fragment = (FishFragment) getSupportFragmentManager()
+                     .findFragmentByTag("fishFragment");
 
-         if (fragment == null) {
-             fragment = new FishFragment();
+             if (fragment == null) {
+                 fragment = new FishFragment();
+             }
+
+             Bundle bundle = new Bundle();
+             bundle.putParcelable("fish", fishes[i - 1]);
+             fragment.setArguments(bundle);
+
+             LinearLayout listLayout = findViewById(R.id.fragmentLayout);
+             LinearLayout fragLayout = new LinearLayout(this);
+             int fragId = View.generateViewId();
+             fragLayout.setId(fragId);
+             listLayout.addView(fragLayout);
+
+             FragmentManager manager = getSupportFragmentManager();
+             FragmentTransaction transaction = manager.beginTransaction();
+             transaction.replace(fragId, fragment).commit();
          }
-
-         Bundle bundleA = new Bundle();
-         bundleA.putParcelable("fish", fishes[i]);
-         fragment.setArguments(bundleA);
-
-         FragmentManager fragmentManager = getSupportFragmentManager();
-         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-         fragmentTransaction.add(R.id.fishFragment, fragment).commit();
      }
 
      @Override
