5df9d83e50a1c69289cf7a02ae60f46eb003b3e2
char-318 <charlotte@mulvey.uk.net>
2023-03-25 18:05:24 +0000

    Creating home button on virtual tank page

--- FishCompatibility/app/src/main/java/com/example/fishcompatibility/TankActivity.java
+++ FishCompatibility/app/src/main/java/com/example/fishcompatibility/TankActivity.java
@@ -2,13 +2,20 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class TankActivity extends AppCompatActivity {
+ public class TankActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tank);
     }
+
+     public void openMain(View view) {
+         Intent openMainIntent = new Intent(getApplicationContext(), MainActivity.class);
+         startActivity(openMainIntent);
+     }
 }
